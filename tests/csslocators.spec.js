a80a3af237126de73b110d69a0f276deaaa9f453
CaSandraSmith <casresm3@gmail.com>
2024-01-22 15:41:23 -0500

    Created test to insert cookies and keep user logged in through new contexts

--- tests/csslocators.spec.js
+++ tests/csslocators.spec.js
@@ -40,3 +40,24 @@ test("login test", async({ page }) => {
     await expect(button).toContainText("OPEN MEDIA PLAYER")
 
 })
+
+test("insert cookies into new context to stay logged in", async({ browser }) => {
+    const context = await browser.newContext()
+    context.addCookies([
+        {
+            url: "https://lofy.onrender.com/",
+            name: "session",
+            value: ".eJwljjtqBTEMAO_iOoUl25L8LrPoS0Iggd33qpC7ZyHlTDHMTzvqzOu9PZ7nK9_a8RHt0WoQiO9QHYaMURjuUyxZkZQh0LtI6nBjwwm2UgErVlXnnSu2Kd0RVvZlBSLch_LuWwUlXBDGQKeVFWk91tqLIIjE1cJ2u0deV57_N3CjX2cdz-_P_LoFR02ZPpnIc6QJQxakB5f3XBBFiArQfv8AxMRBNg.Za7KGw.eqGvzSL2Wl0142E062nH-TRMdTs",
+        }, 
+        {
+            url: "https://lofy.onrender.com/",
+            name: "csrf_token",
+            value: "IjdkZjQ4NGM0NzY2Y2UzZWI4NzFlZjFlY2Q3ZmMwZTUxZGY2MjJhMTEi.Za7KHw.rzDfmlI3IXdno9Trdt51jg5O-uA",
+        }
+    ])
+    const page = await context.newPage()
+    await page.goto("https://lofy.onrender.com/")
+
+    let button = page.locator(".slash-page-text>button")
+    await expect(button).toContainText("OPEN MEDIA PLAYER")
+})
