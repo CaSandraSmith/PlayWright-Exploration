a4a0e589f930cd8e5f911eaab6a2f1edbe50e7be
CaSandraSmith <casresm3@gmail.com>
2024-01-22 15:09:03 -0500

    Created test to ensure user can login with valid credentials

--- tests/csslocators.spec.js
+++ tests/csslocators.spec.js
@@ -25,15 +25,18 @@ test("invalid login test", async({page}) => {
     // await expect(error).toContainText("Invalid")
 })
 
-test.only("login test", async({ page }) => {
+test("login test", async({ page }) => {
     await page.goto("https://lofy.onrender.com/login")
 
     await page.locator("input[type='email']").fill("demo@aa.io")
     await page.locator("input[type='password']").fill("password")
     await page.locator(".login-button").click()
 
-    let button = await page.locator("button")
+    // this failed when just given button selector ex.(page.locator("button")) bc PW will run the test if the element is visible on the page
+    // and there were buttons visibile, just not the one that I was specifically looking for
+    let button = page.locator(".slash-page-text>button")
 
-    await expect(button).toContainText("open media player")
+    // this is case sensitive
+    await expect(button).toContainText("OPEN MEDIA PLAYER")
 
 })
