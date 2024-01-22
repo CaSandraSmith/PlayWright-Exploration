dbf60bac2062e5587c9610303da09947ffa4184b
CaSandraSmith <casresm3@gmail.com>
2024-01-22 14:44:22 -0500

    Updated playwright version

--- tests/csslocators.spec.js
+++ tests/csslocators.spec.js
@@ -1,7 +1,8 @@
 const { test, expect } = require('@playwright/test')
+const exp = require('constants')
 
 // locators with css selectors
-test("login test", async({ page }) => {
+test("css selectors practice", async({ page }) => {
     await page.goto("https://lofy.onrender.com/login")
 
     await page.locator("input[type='email']").fill("demo@aa.com")
@@ -10,16 +11,29 @@ test("login test", async({ page }) => {
 
 })
 
-test.only("invalid login test", async({page}) => {
+test("invalid login test", async({page}) => {
     await page.goto("https://lofy.onrender.com/login")
 
-    await page.locator("input[type='email']").fill("demo@aagggg.com")
+    await page.locator("input[type='email']").fill("demo@aa.com")
     await page.locator("input[type='password']").fill("password")
     await page.locator(".login-button").click()
 
-    let error = await page.locator(".login-form-errors")
+    // await only required when performing action
+    let error = page.locator(".login-form-errors")
     await expect(error).toContainText("Invalid credentials.")
     // substring is okay
     // await expect(error).toContainText("Invalid")
 })
 
+test.only("login test", async({ page }) => {
+    await page.goto("https://lofy.onrender.com/login")
+
+    await page.locator("input[type='email']").fill("demo@aa.io")
+    await page.locator("input[type='password']").fill("password")
+    await page.locator(".login-button").click()
+
+    let button = await page.locator("button")
+
+    await expect(button).toContainText("open media player")
+
+})
