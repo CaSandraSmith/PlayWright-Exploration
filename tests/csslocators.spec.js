e18f49c00862f11ce07ed69923671fe4570f0794
CaSandraSmith <casresm3@gmail.com>
2024-01-23 11:35:01 -0500

    Created test to find results based on its index when multiple elements are found

--- tests/csslocators.spec.js
+++ tests/csslocators.spec.js
@@ -61,3 +61,22 @@ test("insert cookies into new context to stay logged in", async({ browser }) => 
     let button = page.locator(".slash-page-text>button")
     await expect(button).toContainText("OPEN MEDIA PLAYER")
 })
+
+test("filtering results for first element with multiple elements returned", async({page}) => {
+    await page.goto("https://lofy.onrender.com/login")
+    await page.locator(".demo-login-button").click()
+    await page.locator(".slash-page-text>button").click()
+
+    // let first = await page.locator(".home-single-album-wrapper > p").nth(0)
+    let firstAlbum = await page.locator(".home-single-album-wrapper > p").first()
+    await expect(firstAlbum).toContainText("Nostalgic Reverie")
+})
+
+test.only("filtering results for nth element with multiple elements returned", async({page}) => {
+    await page.goto("https://lofy.onrender.com/login")
+    await page.locator(".demo-login-button").click()
+    await page.locator(".slash-page-text>button").click()
+
+    let secondAlbumTitle = await page.locator(".home-single-album-wrapper > p").nth(2)
+    await expect(secondAlbumTitle).toContainText("Chillwaves and Coffee")
+})
