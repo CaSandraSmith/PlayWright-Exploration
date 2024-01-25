229c1ab4a7266fa272565a2861a63e92d7fa68f7
CaSandraSmith <casresm3@gmail.com>
2024-01-24 19:52:36 -0500

    Created tests to experiment with the getBy locators

--- tests/getByLocators.spec.js
+++ tests/getByLocators.spec.js
@@ -22,3 +22,21 @@ test("experimenting with getByPlaceholder", async({ page }) => {
     passwordInput.fill("password")
     await expect(passwordInput).toHaveValue("password")
 })
+
+test("experimenting with getByRole and getByText", async({ page }) => {
+    await page.goto("https://rahulshettyacademy.com/angularpractice/")
+    await page.locator("[name='name'].form-control").fill("Ca'Sandra")
+    await page.locator("[name='email']").fill("ca@gmail.com")
+    await page.getByPlaceholder("Password").fill("password")
+    await page.getByLabel("Check me out").click()
+    await page.getByLabel("Gender").selectOption("Female")
+    await page.getByLabel("Student").click()
+    await page.locator("[name='bday']").pressSequentially("09301998")
+    
+    // reminder: input with types submit are treated as submit buttons
+    // await page.getByRole("button", {value : "Submit"}).click()
+    // or
+    await page.getByRole("button", {name : "Submit"}).click()
+
+    await expect(page.getByText("Success! The Form has been submitted successfully!.")).toBeVisible()
+})
