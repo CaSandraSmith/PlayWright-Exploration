f7b82b9b75c14887b8e3be6aa0ff2f8b6fa50bf1
CaSandraSmith <casresm3@gmail.com>
2024-02-05 18:51:45 -0500

    Added assertions to check logged in status after initialization with cookies

--- tests/apiTesting.spec.js
+++ tests/apiTesting.spec.js
@@ -1,60 +1,9 @@
 const { test, expect, request } = require('@playwright/test')
 let token
 
-
 // the before all method will execute once before the rest of the
 // tests execute
-// test.beforeAll(async ({ page }) => {
-//     page.addInitScript(val => {
-//         window.localStorage.setItem('token', val)
-//     }, token)
-
-//     await page.goto("https://rahulshettyacademy.com/client/")
-
-//     // this makes sure that the content has finished loading b4 trying to
-//     // interact with it
-//     await page.locator(".card-body").nth(2).textContent()
-//     let products = page.locator(".card-body")
-
-//     let count = await products.count()
-
-//     for (let i = 0; i < count; i++) {
-//         let current = products.nth(i)
-//         // PW allows up to chain locators and the locators scope is bound
-//         // to the tings contained by the locator in front of it
-//         // so in this case we pulled the products from what was contained
-//         // on the page and then we can pull the name from what is the scope
-//         // of the product
-//         let name = await current.locator("b").textContent()
-
-//         if (name === "IPHONE 13 PRO") {
-//             // finds element with the text add to cart which is the button
-//             await current.locator("text= Add To Cart").click()
-//             break
-//         }
-//     }
-
-//     // this is with a wildcard reg exp
-//     // await page.locator("[routerlink*='cart']").click()
-//     await page.locator("[routerlink='/dashboard/cart']").click()
-
-//     // this didn't work because these values don't show up on the page as soon as clicked
-//     // and these assertions and locators don't have the auto wait capability
-//     // let inCart = await page.locator("h3:has-text('IPHONE 13 PRO')").isVisible()
-//     // await expect(inCart).toBeVisible()
-
-//     await expect(page.locator("h3:has-text('IPHONE 13 PRO')")).toBeVisible()
-
-//     // you can also use this:
-//     // await page.locator("div li").first().waitFor()
-//     // this will make PW wait for these elements to be loaded before running
-//     // these tests
-//     // let inCart = await page.locator("h3:has-text('IPHONE 13 PRO')").isVisible()
-//     // await expect(inCart).toBeVisible()
-// })
-
-
-test("api testing 1", async () => {
+test.beforeAll(async () => {
     let apiContext = await request.newContext()
     // inside of the newContext method, you can send information that you wish
     // to send by default, you can load the base url, give headers, etc
@@ -82,7 +31,23 @@ test("api testing 1", async () => {
 
 })
 
-test("login api test with lofy", async ( ) => {
+
+test("login with cookies found from API", async ({ page }) => {
+    await page.addInitScript(val => {
+        window.localStorage.setItem('token', val)
+    }, token)
+    // this is an initialization script which will be evaluated whenever
+    // the page is navigate or a childframe is attached or navigated
+    
+    await page.goto("https://rahulshettyacademy.com/client/")
+    let button = page.locator("li > button:has-text('Sign Out')")
+
+
+    await expect(button).toBeVisible()
+
+})
+
+test("login api test with lofy", async () => {
     let apiContext = await request.newContext()
 
     await apiContext.get("https://lofy.onrender.com/api/auth")
