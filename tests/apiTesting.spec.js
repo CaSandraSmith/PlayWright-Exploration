8e58d8ae95b7cff3c31fb7164a4e6dc6e7210ab6
CaSandraSmith <casresm3@gmail.com>
2024-02-06 17:03:23 -0500

    Created api call to create order

--- tests/apiTesting.spec.js
+++ tests/apiTesting.spec.js
@@ -11,6 +11,7 @@ test.beforeAll(async () => {
     // instead of having pages, we have to make api calls
     let loginRespone = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login",
         {
+            // this is the login payload
             data: {
                 userEmail: "casresm3@gmail.com",
                 userPassword: "Password1"
@@ -29,16 +30,36 @@ test.beforeAll(async () => {
     token = jsonResponse.token
     // gets the token that we need for our cookies to stay logged in
 
+    let orderResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/order/create-order",
+        {
+            // this is the payload required to make an order
+            data: {orders: [{
+                country: "Algeria",
+                productOrderedId: "6581ca399fd99c85e8ee7f45"
+            }]},
+            headers: {
+                // this application requires that the token is sent with the header authorization
+                // to post to the users account, this can vary based on how the actual API is
+                // written and needs to be varified with dev team
+                'Authorization': token,
+                'Content-Type': "application/json"
+            }
+        },
+    )
+
+    let orderJson = await orderResponse.json()
+    console.log("order", orderJson)
+
 })
 
 
-test("login with cookies found from API", async ({ page }) => {
+test.only("login with cookies found from API", async ({ page }) => {
     await page.addInitScript(val => {
         window.localStorage.setItem('token', val)
     }, token)
     // this is an initialization script which will be evaluated whenever
     // the page is navigate or a childframe is attached or navigated
-    
+
     await page.goto("https://rahulshettyacademy.com/client/")
     let button = page.locator("li > button:has-text('Sign Out')")
 
@@ -46,6 +67,7 @@ test("login with cookies found from API", async ({ page }) => {
     await expect(button).toBeVisible()
 
 })
+
 
 test("login api test with lofy", async () => {
     let apiContext = await request.newContext()
