5b019358dfa7241510e76912c53168ec73727277
CaSandraSmith <casresm3@gmail.com>
2024-02-12 13:55:13 -0500

    Added network interception for order response

--- tests/networkInterception.spec.js
+++ tests/networkInterception.spec.js
@@ -2,7 +2,8 @@ const { test, expect } = require("@playwright/test")
 import APIUtils from "./utils/apiUtils"
 let loginPayload = { userEmail: "casresm3@gmail.com", userPassword: "Password1" }
 let orderPayload = { orders: [{ country: "Algeria", productOrderedId: "6581ca399fd99c85e8ee7f45" }] }
-let response
+let fakePayload = {data: [], message: "No Orders"}
+// this is how the api responsds to users who don't have orders
 
 test.beforeAll(async () => {
     let apiContext = await request.newContext()
@@ -15,6 +16,30 @@ test("Orders are empty with network interception", async ({ page }) => {
         window.localStorage.setItem('token', val)
     }, response.token)
     await page.goto("https://rahulshettyacademy.com/client/")
+
+
+    // this handler allows us to modify the network requests that are made by a single page
+    // the first arg is the url you want to route and second argument is a function
+    // how this works is that the API will send back the correct response, but we will change
+    // the response that the browser actually sees as we cant change how the api resonds, we can only
+    // control the browser
+    await page.route("https://rahulshettyacademy.com/api/ecom/order/get-orders-for-customer/65b14f51a86f8f74dc6080d2", 
+        async(route) => {
+            let response = await page.request.fetch(route.request())
+            // the request helper allows us to use our page to make api calls
+            // from there we are fetching the reaponse from our api at this route
+            // route.request() makes the request manually and the fetch will fetch the
+            // response from the api; we are catching the response
+            await route.fulfill({
+                response,
+                body: fakePayload
+            })
+            // this is where a response is sent back to the browser and then it is rendered based on its content
+            // where we are writing over the initial body of the response with our fake payload to make it look
+            // like the user has no orders, if we put nothing inside of the fufill method, then it would have
+            // responded with the normal response from the api with no modifications
+        }
+    )
 
 
     await expect(button).toBeVisible()
