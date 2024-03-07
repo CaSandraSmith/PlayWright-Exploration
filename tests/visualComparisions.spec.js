243a00aa14fd4911a897ea547b636248415dfd05
CaSandraSmith <casresm3@gmail.com>
2024-03-07 15:42:42 -0500

    Wrote tests for visual comparisons

--- tests/visualComparisions.spec.js
+++ tests/visualComparisions.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test")
 
-test('screenshoots of whole page', async({page}) => {
+test('screenshots of whole page', async({page}) => {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/")
     await expect(page.locator("#displayed-text")).toBeVisible()
     await page.locator("#hide-textbox").click()
@@ -17,4 +17,15 @@ test('screenshots of individual elements', async({page}) => {
     
     await page.locator("#hide-textbox").click()
     await expect(page.locator("#displayed-text")).toBeHidden()
+})
+
+test.only('visual comparison between screenshots', async({page}) => {
+    await page.goto("https://lofy.onrender.com/")
+    await page.locator(".circle-two").waitFor()
+    // if you don't have a screenshot to compare this to it at this location this test will initially fail
+    // but then store the screen shot that it was trying to compare in the location for future tests
+    expect(await page.screenshot()).toMatchSnapshot("screenshots/lofyLanding.png")
+
+    // this is a better way to compare screenshots
+    // await expect(page).toHaveScreenshot("screenshots/lofyLanding2.png")
 })
